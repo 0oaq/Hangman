62b1b30babdb190aa56350ff9bb1fa31aee62623
0oaq <otbestimt@students.wcpss.net>
2022-09-15 06:52:02 -0400

    commit

--- Hangman.js
+++ Hangman.js
@@ -1,33 +1,8 @@
+const guessBox = document.getElementById("guessBox");
 let Human = document.getElementById("manual")
 let Computer = document.getElementById("computer")
-
-
-
-
-if (document.URL.includes("Hangman.html?userFlag=0")) {
-
-
-
-    let wordInput = prompt("hello");
-
-
-    //console.log(wordInput)
-
-
-    function setGuessWord(InNewString) {
-        let mysteryWord = document.getElementById("changingWord");
-        mysteryWord.innerHTML = InNewString;
-    }
-
-    setGuessWord(wordInput);
-
-    console.log(wordInput.length)
-}
-
-
-
-
-
+let mysteryWord = document.getElementById("changingWord");
+let guessClicked = false;
 let bodyPieces = [
     { name: "head", orderIndex: 0 },
     { name: "torso", orderIndex: 1 },
@@ -37,12 +12,6 @@ let bodyPieces = [
     { name: "rightArm", orderIndex: 5 },
 ]
 
-
-const guessBox = document.getElementById("guessBox");
-
-
-let guessClicked = false;
-
 function onGuessClick(letter) {
     if (guessClicked == false) {
         guessBox.value = (letter);
@@ -50,6 +19,38 @@ function onGuessClick(letter) {
     guessClicked = true;
 }
 
+if (document.URL.includes("Hangman.html?userFlag=0")){
+
+    let wordInput = prompt("hello");
+
+    function setGuessWord(InNewString) {
+        mysteryWord.innerHTML = InNewString; 
+    }
+    setGuessWord(wordInput);
+
+    console.log(wordInput.length)
+}
+
+let testWord ="Foo Bar"
+for (let i=0; i<testWord.length; i++)
+{
+    let curChar = testWord.charAt(i);
+    
+    let bLowerCaseLetter = (curChar >= 'a') && (curChar <='z');
+    let bUpperCaseLetter = (curChar >= 'A') && (curChar <= 'Z');
+    if(bLowerCaseLetter || bUpperCaseLetter){
+        curChar = "_"
+    }
+
+
+
+    console.log(curChar);
+
+}
+const reg = /[a-zA-Z]/g
+let testWordReplace = testWord.replace(reg, "_")
+console.log("FFFFFFFUFFFFF");
+console.log(testWordReplace);
 
 /*
 Add edit box for new key
